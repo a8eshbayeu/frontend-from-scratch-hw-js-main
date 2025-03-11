9121ab65ac14b6c276b5f3716c2c8941f87ae54a
Artur Eshbayeu <a.eshbayeu@gmail.com>
2025-03-11 19:03:24 +0000

    seventh commit

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,20 +7,22 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
+
 let dice1 = Math.floor(Math.random() * (6 - 1 + 1)) + 1; // your code
 let dice2 = Math.floor(Math.random() * (6 - 1 + 1)) + 1; // your code
 let isWinningDouble;
 
 if (dice1 > 3 && dice2 > 3 && dice1 === dice2) {
   isWinningDouble = true;
-
-
-
-console.log('Первый бросок: ' + dice1)
-console.log('Второй бросок: ' + dice2)
-if (isWinningDouble) {
-  console.log('Выигрышный дубль!')
 } else {
-  console.log('Не выигрышный дубль.')
+  isWinningDouble = false;
 }
-}
+  // your code
+
+  console.log("Первый бросок: " + dice1);
+  console.log("Второй бросок: " + dice2);
+  if (isWinningDouble) {
+    console.log("Выигрышный дубль!");
+  } else {
+    console.log("Не выигрышный дубль.");
+  }
