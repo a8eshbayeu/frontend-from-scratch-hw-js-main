e258db00c8d4e8bb093835a5887a147bf5ca0fdc
Artur Eshbayeu <a.eshbayeu@gmail.com>
2025-04-06 18:12:14 +0200

    Update 01.js

--- lesson-06/01.js
+++ lesson-06/01.js
@@ -13,9 +13,9 @@
 */
 
 const WEB_TECH_IMAGES = [
-  'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/32f74d50-68d0-46aa-b035-7b3a5300d2c1_js-magic-logo.jpg',
-  'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
-  'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
+    'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/32f74d50-68d0-46aa-b035-7b3a5300d2c1_js-magic-logo.jpg',
+    'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
+    'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
 ]
 // Создаём элементы слайдера
 
@@ -36,9 +36,9 @@ mySlider.append(slide_2)
 const slide_3 = document.createElement("img")
 slide_3.setAttribute("src", WEB_TECH_IMAGES[2])
 slide_3.classList.add('slide')
-mySlider.append(slide_2)
+mySlider.append(slide_3)
 
-const slides = Array.from(mySlider.querySelectorAll('.slide'))
+const slides = Array.from(mySlider.querySelectorAll('img'))
 
 const slideCount = slides.length
 let slideIndex = 0
@@ -56,27 +56,27 @@ document.body.append(nextBtn)
 // Функция для показа предыдущего слайда
 
 function showPrevSlide() {
-  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
-  updateSlider()
+   slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+   updateSlider()
 }
 
 // Функция для показа следующего слайда
 
 function showNextSlide() {
-  slideIndex = (slideIndex + 1) % slideCount;
-  updateSlider()
+    slideIndex = (slideIndex + 1) % slideCount;
+    updateSlider()
 }
 
 // Функция для обновления отображения слайдера
 
 function updateSlider() {
-  slides.forEach((slide, index) => {
-    if(index === slideIndex) {
-      slide.style.display = 'block'
-    } else {
-      slide.style.display = 'none'
-    }
-  })
+    slides.forEach((slide, index) => {
+        if(index === slideIndex) {
+            slide.style.display = 'block'
+        } else {
+            slide.style.display = 'none'
+        }
+    })
 }
 
 updateSlider()
