59b2c25b8b0093a9d4067c0edf430c0fa3b6de26
Artur Eshbayeu <a.eshbayeu@gmail.com>
2025-04-26 13:39:57 +0200

    Update 02.js

--- lesson-09/02.js
+++ lesson-09/02.js
@@ -24,4 +24,13 @@ const doubledNumbers = map(numbers, (element, index) => {
 console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
 */
 
-const map = () => {}
+const map = (array, callback) => {
+    const result = []
+for (let index = 0; index < array.length; index++) {
+const element = array[index]
+    if(callback(element, index)) {
+        result.push(element)
+    }
+}
+return result
+}
